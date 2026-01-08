28a0f0fc0cc41f483729af3405dc0f6cc07b2416
OBB-06 <olayabaabidane@gmail.com>
2026-01-08 18:54:47 +0100

    ruta

--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,16 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-// ruta para probar el middleware
+// ruta sin middleware
+Route::get('/home', function () {
+    return 'Pagina de inicio - acceso libre';
+});
+
+// rutas con middleware
+Route::get('/adultos', function () {
+    return 'Bienvenido al area de adultos!';
+})->middleware('age');
+
 Route::get('/prueba', function () {
     return 'Eres mayor de edad!';
 })->middleware('age');
